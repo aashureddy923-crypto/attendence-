ba388f9b60a2d201a880a9a7c7a28c3ff657e777
Aashritha <aashureddy923@gmail.com>
2026-06-18 10:59:43 +0530

    files move to repo

---
+++ frontend/register.js
@@ -0,0 +1,31 @@
+console.log("register.js loaded");
+const regBtn = document.getElementById('register-button');
+//console.log(regBtn); //rectify the error
+regBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    const username=document.getElementById("username");
+    const email=document.getElementById("email");
+    const password=document.getElementById("password");
+    const confirmPassword=document.getElementById("confirmPassword");
+    if(username.value==="" || email.value==="" || password.value==="" || confirmPassword.value===""){
+        alert("Please fill all the fields");
+        return;
+    }
+    if(password.value!==confirmPassword.value){
+        alert("Passwords do not match");
+        return;
+    }
+    const user={
+        name: username.value,
+        email: email.value,
+        password: password.value
+    }
+    let users=JSON.parse(localStorage.getItem("users") || "[]")
+    users.push(user)   
+    console.log(users)
+    localStorage.setItem("users", JSON.stringify(users))
+    localStorage.setItem("lastRegisteredEmail", email.value);
+    alert("Registration successful")
+    window.location.href="login.html"
+
+})
